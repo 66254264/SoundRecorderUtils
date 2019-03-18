364a35d61eafbbf3fd88bf88c16c69f2c8a7bd08
IT_Hz <66254264@qq.com>
2019-03-18 14:59:50 +0800

    英文转语音
    Signed-off-by: IT_Hz <66254264@qq.com>

--- app/src/main/java/com/sid/soundrecorderutils/MainActivity.java
+++ app/src/main/java/com/sid/soundrecorderutils/MainActivity.java
@@ -2,36 +2,70 @@ package com.sid.soundrecorderutils;
 
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.gson.JsonObject;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG ="testjson" ;
     private Button mBtnRecordAudio;
     private Button mBtnPlayAudio;
-    private WebView wvCustom;
     public static final int RESULT_CODE_STARTAUDIO=10102;
+    private EditText text;
+    private Button readButton;
+    private TextToSpeech tts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        //initWebView();
+        //初始化TTS
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                // 判断是否转化成功
+                if (status == TextToSpeech.SUCCESS){
+                    //默认设定语言为中文，原生的android貌似不支持中文。
+                    int result = tts.setLanguage(Locale.CHINESE);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                        Toast.makeText(MainActivity.this, "数据丢失或不支持", Toast.LENGTH_SHORT).show();
+                    }else{
+                        //不支持中文就将语言设置为英文
+                        tts.setLanguage(Locale.US);
+                    }
+                }
+            }
+        });
+        //文字转语音
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = text.getText().toString().trim();
+                if (!TextUtils.isEmpty(str)){
+                    // 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+                    tts.setPitch(1.0f);
+                    // 设置语速
+                    tts.setSpeechRate(1.0f);
+                    //播放语音
+                    tts.speak(str, TextToSpeech.QUEUE_ADD, null);
+                }
+
+            }
+        });
+        //录音
         mBtnRecordAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-
+        //播放
         mBtnPlayAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,73 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initWebView() {
-        wvCustom.clearCache(true);
-        WebSettings webSettings = wvCustom.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setPluginState(WebSettings.PluginState.ON);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setAppCacheMaxSize(1024 * 1024 * 8);
-        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
-        webSettings.setAppCachePath(appCachePath);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            webSettings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-        webSettings.setSupportZoom(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setLoadsImagesAutomatically(true);
-        wvCustom.addJavascriptInterface(new Object() {
 
-            //录制音频
-            @JavascriptInterface
-            public void RecordAudio(){
-                final RecordAudioDialogFragment fragment = RecordAudioDialogFragment.newInstance();
-                fragment.show(getSupportFragmentManager(), RecordAudioDialogFragment.class.getSimpleName());
-                fragment.setOnCancelListener(new RecordAudioDialogFragment.OnAudioCancelListener() {
-                    @Override
-                    public void onCancel() {
-                        fragment.dismiss();
-                    }
-                });
-            }
-            //结束录制音频
-            @JavascriptInterface
-            public String finishRecordAudio(){
-                SharedPreferences sharePreferences = getSharedPreferences("sp_name_audio", MODE_PRIVATE);
-                final String filePath = sharePreferences.getString("audio_path", "");
-                String audioBytes = Utils.FileToStr(filePath);
-                JsonObject json = new JsonObject();
-                json.addProperty("data",audioBytes);
-                json.addProperty("type","mp4");
-                Log.i(TAG, "finishRecordAudio: "+json.toString());
-                return json.toString();
-            }
-            //播放音频
-            @JavascriptInterface
-            public void PlayAudio(){
-                RecordingItem recordingItem = new RecordingItem();
-                SharedPreferences sharePreferences = getSharedPreferences("sp_name_audio", MODE_PRIVATE);
-                final String filePath = sharePreferences.getString("audio_path", "");
-                long elpased = sharePreferences.getLong("elpased", 0);
-                recordingItem.setFilePath(filePath);
-                recordingItem.setLength((int) elpased);
-                PlaybackDialogFragment fragmentPlay = PlaybackDialogFragment.newInstance(recordingItem);
-                fragmentPlay.show(getSupportFragmentManager(), PlaybackDialogFragment.class.getSimpleName());
-            }
-
-            @JavascriptInterface
-            public void finishActivity() {
-                finish();
-            }
-
-        }, "android");
-        wvCustom.loadUrl("http://172.18.158.34:8080/demo");
-        wvCustom.setWebChromeClient(new WebChromeClient());
-    }
 
 
 
@@ -157,6 +125,17 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mBtnRecordAudio = (Button)findViewById(R.id.main_btn_record_sound);
         mBtnPlayAudio = (Button) findViewById(R.id.main_btn_play_sound);
-        wvCustom= (WebView) findViewById(R.id.wv_web_test);
+        text = (EditText) findViewById(R.id.et_text);
+        readButton = (Button) findViewById(R.id.read);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //tts注销
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
     }
 }
